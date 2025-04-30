e3935ba783959ef2e8ff22a0a7d7ced88769ac6a
Tomas Wylie <tomas@Tomass-MacBook-Pro.local>
2025-04-30 17:04:39 +0100

    Initial commit - implementation of Calorimeter object with print function

---
+++ Calorimeter.cpp
@@ -0,0 +1,28 @@
+// Calorimeter.cpp
+
+// Implementation file of the Calorimeter detector
+
+// Student ID: 10831050, last updated: 30/04/25
+
+#include "Calorimeter.h"
+
+// Default constructor
+Calorimeter::Calorimeter() :
+  SubDetector("",""), number_of_crystals{0}
+{};
+
+// Parameterised constructor
+Calorimeter::Calorimeter(std::string detector_type_in, std::string material_in, int number_of_crystals_in) :
+  SubDetector(detector_type_in, material_in), number_of_crystals{number_of_crystals_in}
+{
+  if(!(number_of_crystals_in >= 0)) {throw std::invalid_argument("Number of crystals must be a positive integer.");}
+};
+
+// Destructor
+Calorimeter::~Calorimeter(){};
+
+// Overrides the print_detector function for calorimeter
+void Calorimeter::print_detector() const
+{
+  std::cout<<"\033[1;37mCalorimeter:\033[0m "<<"Material: "<<material<<", Number of crystals: "<<number_of_crystals<<std::endl;
+}
---
+++ Calorimeter.h
@@ -0,0 +1,34 @@
+// Calorimeter.h
+
+// Header file of the Calorimeter detector
+
+// Student ID: 10831050, last updated: 30/04/25
+
+#ifndef CALORIMETER_H
+#define CALORIMETER_H
+
+#include "SubDetector.h"
+
+class Calorimeter: public SubDetector
+{
+protected:
+  int number_of_crystals;
+
+public:
+
+  // Default constructor
+  Calorimeter();
+
+  // Parameterised constructor
+  Calorimeter(std::string detector_type_in, std::string material_in, int number_of_crystals_in);
+
+  // Destructor
+  ~Calorimeter();
+
+  // Overrides the print_data function for calorimeter
+  void print_detector() const;
+
+
+};
+
+#endif
