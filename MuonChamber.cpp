470a89c4b75ca1ac525984fc650b143f01fe88f8
Tomas Wylie <tomas@Tomass-MacBook-Pro.local>
2025-04-30 17:35:20 +0100

    Initial commit

---
+++ MuonChamber.cpp
@@ -0,0 +1,7 @@
+// MuonChamber.cpp
+
+// Implementation file of the MuonChamber detector
+
+// Student ID: 10831050, last updated: 30/04/25
+
+#include "MuonChamber.h"
---
+++ MuonChamber.h
@@ -0,0 +1,19 @@
+// MuonChamber.h
+
+// Header file of the MuonChamber detector
+
+// Student ID: 10831050, last updated: 30/04/25
+
+#ifndef MUONCHAMBER_H
+#define MUONCHAMBER_H
+
+#include "SubDetector.h"
+
+class MuonChamber: public SubDetector
+{
+protected:
+  
+public:
+};
+
+#endif
